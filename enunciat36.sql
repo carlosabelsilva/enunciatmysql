3643b08523174dbb8f47c437480ac753bca4d7f7
carlossilva <carlosabelsilva2002@ginebro.cat>
2020-12-09 17:16:22 +0100

    first commit

---
+++ enunciat36.sql
@@ -0,0 +1,13 @@
+mysql> SELECT titol_peli, recaudacio_peli FROM PELLICULES ORDER BY recaudacio_peli DESC LIMIT 1;
+--------------
+SELECT titol_peli, recaudacio_peli FROM PELLICULES ORDER BY recaudacio_peli DESC LIMIT 1
+--------------
+
++----------------+-----------------+
+| titol_peli     | recaudacio_peli |
++----------------+-----------------+
+| Los Vengadores |      1519557910 |
++----------------+-----------------+
+1 row in set (0,00 sec)
+
+mysql> quit
